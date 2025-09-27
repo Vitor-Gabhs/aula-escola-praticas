95abf64f20874e3bdaa251ae144e23d078133ae0
Vitor-Gabhs <vitor.dutra.marques@escola.pr.gov.br>
2025-09-27 14:10:38 -0300

    Create verificaIdade.js

---
+++ verificaIdade.js
@@ -0,0 +1,9 @@
+let idade = parseInt(prompt("digite sua idade:"));
+let veredito;
+
+if (idade >= 18){
+ veredito = "você é maior de idade!";
+} else {
+ veredito = "você é menor de idade.";
+}
+console.log(veredito);
