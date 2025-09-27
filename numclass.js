d739cf575aff31886275c44ae01b1bb0a003334f
Vitor-Gabhs <vitor.dutra.marques@escola.pr.gov.br>
2025-09-27 15:02:20 -0300

    Create numclass.js

---
+++ numclass.js
@@ -0,0 +1,8 @@
+let num = parseInt(prompt("digite um número:" ));
+if (num > 0){
+    console.log("é positivo.");
+} else if (num < 0) {
+    console.log("é negativo");
+} else {
+    console.log("é zero");
+}
