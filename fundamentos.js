a8bf4c5ecc8bd5cffdd65bf195def7df1a8a869d
Vitor-Gabhs <vitor.dutra.marques@escola.pr.gov.br>
2025-09-27 14:01:03 -0300

    fundamentos.js

--- fundamentos.js
+++ fundamentos.js
@@ -1,16 +1,16 @@
 //1.
-let idade 
-idade = 30 
-console.log(idade)
+let idade;
+idade = 30;
+console.log(idade);
 
 //2.
 function soma(n1, n2) {
     return n1+n2;
 }
-let n1 = 1
-let n2 = 2
-let resultado = soma(n1,n2)
-console.log(resultado)
+let n1 = 1;
+let n2 = 2;
+let resultado = soma(n1,n2);
+console.log(resultado);
 
 //3.
 let num = prompt("digite um número:");
@@ -19,24 +19,24 @@ let resultado;
 if (numR % 2 === 0){
     resultado = "é par!";
 } else {
-    resultado = "é ímpar!"
+    resultado = "é ímpar!";
 }
 console.log("seu número", resultado)
 
 //4.
 let n1 = parseInt(prompt("digite três números para uma média:")), n2 = parseInt(prompt()), n3 = parseInt(prompt());
-let calc = ((n1 + n2 + n3) /3)
-console.log("resultado é:", calc)
+let calc = ((n1 + n2 + n3) /3);
+console.log("resultado é:", calc);
 
 //5.
-console.log("Olá Mundo!")
+console.log("Olá Mundo!");
 
 
 //6.
 let i = 5
 while (i >= 1) {
- console.log(i)
- i--
+ console.log(i);
+ i--;
 }
 
 //7.
