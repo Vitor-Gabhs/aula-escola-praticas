cb961c68862620f2e3638b7c9826dbe536f8f632
Vitor-Gabhs <vitor.dutra.marques@escola.pr.gov.br>
2025-09-27 15:11:26 -0300

    Create validaCompra.js

---
+++ validaCompra.js
@@ -0,0 +1,8 @@
+let saldo = 100;
+let valor = 250;
+let calc = saldo - valor;
+if (saldo >= valor){
+    console.log("sua compra foi aprovada, seu saldo é:", calc);
+} else {
+    console.log("compra recusada");
+}
