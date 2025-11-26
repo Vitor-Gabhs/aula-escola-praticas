863019e6cb6f93e9989a41d1a67aa205664aa1c0
Vitor-Gabhs <vitor.dutra.marques@escola.pr.gov.br>
2025-11-26 14:37:10 -0300

    Create 1pessoa.js

---
+++ 1pessoa.js
@@ -0,0 +1,14 @@
+class Pessoa {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  apresentar() {
+    console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
+  }
+}
+
+const pessoaTeste = new Pessoa("Ana Silva", 30);
+
+pessoaTeste.apresentar();
